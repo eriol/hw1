c61c099bb7f59a0d4e3455c48fef99c64c4db3ed
Daniele Tricoli <eriol@mornie.org>
2024-05-15 02:31:51 +0200

    Split header and footer

---
+++ footer.php
@@ -0,0 +1,68 @@
+    <footer>
+      <div id="footer_container">
+        <div class="social">
+          <div class="logo has-primary-color is-bold">ἀθλητική</div>
+          <div>
+            <p class="disclaimer">ἀθλητική protegge i tuoi dati.</p>
+            <div id="social_logos">
+              <img class="social_logo" src="images/cloud-solid.svg" />
+              <img class="social_logo" src="images/mountain-solid.svg" />
+              <img class="social_logo" src="images/bolt-solid.svg" />
+              <img class="social_logo" src="images/gopuram-solid.svg" />
+            </div>
+          </div>
+        </div>
+        <div class="footer_links">
+          <ul class="links">
+            <li>
+              <a href="#">Funzioni</a>
+            </li>
+            <li>
+              <a href="#">Abbonati</a>
+            </li>
+            <li>
+              <a href="#">Sconti per discenti</a>
+            </li>
+            <li>
+              <a href="#">Lavora con noi</a>
+            </li>
+            <li>
+              <a href="#">Informazioni</a>
+            </li>
+            <li>
+              <a href="#">Stampa</a>
+            </li>
+          </ul>
+          <ul class="links">
+            <li>
+              <a href="#">Percorsi</a>
+            </li>
+          </ul>
+          <ul class="links">
+            <li>
+              <a href="#">Cosa c'è di nuovo</a>
+            </li>
+            <li>
+              <a href="#">Assistenza</a>
+            </li>
+            <li>
+              <a href="#">Termini</a>
+            </li>
+          </ul>
+          <div class="flex-break"></div>
+          <ul class="links">
+            <li>
+              <a href="#">Non voglio condividere i miei dati!</a>
+            </li>
+            <li>
+              <a href="#">Privacy</a>
+            </li>
+            <li>
+              <a class="has-primary-color" href="#">Accedi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title><?php echo $title;?></title>
+    <link rel="stylesheet" href="css/hw1.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <script src="js/hw1.js" defer></script>
+  </head>
+  <body>
+    <header>
+      <nav id="navtop">
+        <div id="logo_and_links">
+          <a id="menu">
+            <img src="images/bars-solid.svg" data-state="open" />
+          </a>
+          <div class="logo has-primary-color is-bold">ἀθλητική</div>
+          <ul class="links">
+            <li>
+              <a href="#">Funzioni</a>
+            </li>
+            <li>
+              <a href="#">Sfide</a>
+            </li>
+            <li>
+              <a href="#">Abbonati</a>
+            </li>
+          </ul>
+        </div>
+        <a class="button">Accedi</a>
+      </nav>
+      <section id="menu_panel" class="">
+        <nav id="navleft">
+          <div id="panel_links">
+            <ul class="links text-big">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Funzioni</a>
+              </li>
+              <li>
+                <a href="#">Sfide</a>
+              </li>
+            </ul>
+            <hr />
+            <ul class="links text-normal">
+              <li>
+                <a href="#">Abbonati</a>
+              </li>
+              <li>
+                <a href="#">Assistenza</a>
+              </li>
+            </ul>
+            <a class="button button-wide">Accedi</a>
+            <a class="button button-wide account_register_email">
+              Iscriviti con l'indirizzo email
+            </a>
+          </div>
+        </nav>
+      </section>
+      <section id="menu_modal" class="hidden"></section>
+    </header>
