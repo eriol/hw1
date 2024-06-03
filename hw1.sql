8ab70c7324929118a4a295e6d3f305ddaeeecec3
Daniele Tricoli <eriol@mornie.org>
2024-06-03 23:41:18 +0200

    Add activities table

--- hw1.sql
+++ hw1.sql
@@ -18,6 +18,30 @@ CREATE TABLE IF NOT EXISTS profiles (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS sports (
+    id VARCHAR(100) NOT NULL PRIMARY KEY,
+    name VARCHAR(200)
+);
+
+CREATE TABLE IF NOT EXISTS activities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    performance FLOAT,
+    sport_id VARCHAR(100),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (sport_id) REFERENCES sports(id) ON DELETE CASCADE
+);
+
+INSERT IGNORE INTO sports (id, name) VALUES ('pugilato', 'Pugilato');
+INSERT IGNORE INTO sports (id, name) VALUES ('corsa', 'Corsa');
+INSERT IGNORE INTO sports (id, name) VALUES ('lotta', 'Lotta');
+INSERT IGNORE INTO sports (id, name) VALUES ('danza', 'Danza');
+INSERT IGNORE INTO sports (id, name) VALUES ('lancio-del-disco', 'Lancio del disco');
+INSERT IGNORE INTO sports (id, name) VALUES ('salto-in-lungo', 'Salto in lungo');
+INSERT IGNORE INTO sports (id, name) VALUES ('lancio-del-giavellotto', 'Lancio del giavellotto');
+
 CREATE TRIGGER empty_profile AFTER INSERT ON users
     FOR EACH ROW
     INSERT INTO profiles (user_id) VALUES (NEW.id);
