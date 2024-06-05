07b6063617bee657e85b4a3ca0c5b7a59a747a2c
Daniele Tricoli <eriol@mornie.org>
2024-06-05 15:42:01 +0200

    Check if sport exist

--- activities/api/database.php
+++ activities/api/database.php
@@ -16,4 +16,18 @@
         return $sports;
     }
 
+    function do_sport_exist($conn, $sport) {
+
+        $sport = mysqli_real_escape_string($conn, $sport);
+        $query = "SELECT id FROM sports id = '$sport'";
+
+        $rows = mysqli_query($conn, $query);
+        if (mysqli_num_rows($rows) > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+
 ?>
